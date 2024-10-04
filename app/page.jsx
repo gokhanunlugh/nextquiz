f26acd162f354128a96acc3b93fa7be377b117ec
Gokhan <gokhanunlu94@gmail.com>
2024-10-04 14:48:19 +0300

    kategori seçim fonksiyonu eklendi. seçilen kategori logosu yakalandı

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,14 @@
+"use client"
 import data from '@/app/data.json'
-import { useState } from 'react/cjs/react.production.min';
+import { useState } from 'react';
 
 export default function Home() {
   const [genre, setGenre] = useState([]);
+  const {question, setQuestion} = useState(0);
+
+  function handleClick(x){
+    setGenre(x);
+  }
   return (
     <>
     {
@@ -19,13 +25,30 @@ export default function Home() {
           <p>Pick a subject to get started.</p>
         </div>
         <div className="categories">
-          {data.questionAndAnswers.map((x,i) => <label key={i}> <button><img src={x.icon}/>{x.category}</button></label>)}
+          {data.questionAndAnswers.map((x,i) => <label key={i}> <button onClick={ () => handleClick(x)}><img src={x.icon}/>{x.category}</button></label>)}
         </div>
       </div>
     </div>
     :
-    <Questions />
+    <Questions genre={genre} setGenre={setGenre} question={question} setQuestion={setQuestion} />
     }
     </>
   );
+}
+
+
+function Questions({genre, setGenre, question, setQuestion}) {
+  
+  return (
+    <>
+    <div className="container">
+      <header>
+        <div className="logo">
+          {<img src={genre.icon} />}
+        </div>
+        {}
+      </header>
+    </div>
+    </>
+  )
 }
