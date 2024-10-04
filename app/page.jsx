3fbb195fb126c2f670f043c904a3cf1fe6be2214
Gokhan <gokhanunlu94@gmail.com>
2024-10-04 10:57:36 +0300

    yeni logolar eklendi, datajson yer değiştirildi

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,25 @@
-
+import data from '@/app/data.json'
 
 export default function Home() {
   return (
-    <></>
+    <>
+    <div className="container">
+      <header>
+        <div className="logo"></div>
+        {}
+      </header>
+      <div className="content">
+        <div className="context">
+          <h2>Welcome to the</h2>
+          <h2><strong>Frontend Quiz!</strong></h2>
+          <p>Pick a subject to get started.</p>
+        </div>
+        <div className="categories">
+          {data.questionAndAnswers.map((x,i) => <label key={i}> <button><img src={x.icon}/>{x.category}</button></label>)}
+        </div>
+      </div>
+    </div>
+    
+    </>
   );
 }
